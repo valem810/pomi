7fc9b27035df03a150df6d8dc4e663ce5a8e1406
valem810 <valeria.miranda4168@alumnos.udg.mx>
2024-11-19 00:50:30 -0600

    Prueba

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   build: {
-    outDir: 'dist', // Asegúrate de que esto esté definido correctamente
+    outDir: 'dist', // Carpeta donde se guardarán los archivos de producción
   },
 });
