fc513664d2b722fdab9913d2522440e25f8146e2
valem810 <valeria.miranda4168@alumnos.udg.mx>
2024-11-18 23:53:45 -0600

    Prueba

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'; // Asegúrate de incluir esta línea
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      '@': path.resolve(__dirname, 'src'),
     },
-})
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000, // Cambia el puerto si es necesario
+  },
+});
